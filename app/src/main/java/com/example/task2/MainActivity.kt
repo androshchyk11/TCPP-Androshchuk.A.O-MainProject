a78037b20716dce54a5938f254a30b4057cadb89
androshchyk11 <51484732+androshchyk11@users.noreply.github.com>
2021-04-27 23:00:46 +0300

    initial cpmmit

---
+++ app/src/main/java/com/example/task2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.task2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.e("TAG","Artem")
+        Log.e("TAG","Androshchuk")
+        Log.e("TAG","122-A")
+    }
+}
